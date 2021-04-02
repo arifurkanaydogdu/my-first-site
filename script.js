b640f1949be377b82119c45015c712f53274dcb6
arifurkanaydogdu <81788302+arifurkanaydogdu@users.noreply.github.com>
2021-04-02 03:59:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var button = document.querySelector('button');
   
     button.addEventListener('click', function() {
-      alert("Let's Rock!!");
+      alert("Ben sana aşığım!!");
     });
   }
   
